7fd78dbaf061bdab9d5136f93d60a7ac337ca7e5
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-17 21:31:54 -0300

    fix: change values

--- main.c
+++ main.c
@@ -4,8 +4,10 @@ int	main(void)
 {
 	ft_printf("Valor: %d\n", 42);
 	ft_printf("Valor2: %s\n", "teste");
-	int idade = 29;
-	char *nome = "Rods";
+
+
+	int idade = 33;
+	char *nome = "Antonio";
 
 	ft_printf("Me chamo %s e tenho %i anos!\n", nome, idade);
 	return (0);
