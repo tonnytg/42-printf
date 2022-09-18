e63c145138c0349078736f6614bb187aecbd5d30
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-17 21:42:45 -0300

    fix: move files correct place

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,14 @@
 #include "test.h"
 
-int main(void)
+int	main(void)
 {
+	ft_printf("Valor: %d\n", 42);
+	ft_printf("Valor2: %s\n", "teste");
+
+
+	int idade = 33;
+	char *nome = "Antonio";
+
+	ft_printf("Me chamo %s e tenho %i anos!\n", nome, idade);
 	return (0);
 }
