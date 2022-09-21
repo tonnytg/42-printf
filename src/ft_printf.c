1a17fb06df395db0f09d49a145693d0c01c1a3b5
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-20 23:02:39 -0300

    feat: solve % and c

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -7,17 +7,6 @@ void ft_putchar(int c)
 	write(1, &c, 1);
 }
 
-void ft_putstr(char *str)
-{
-	int i;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
-
 void ft_putnbr(int nb)
 {
 	if (nb < 0)
@@ -32,32 +21,59 @@ void ft_putnbr(int nb)
 	ft_putchar(nb % 10 + '0');
 }
 
-int ft_printf(const char *args, ...)
+void ft_putstr(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int ft_printf(const char *str, ...)
 {
 	int i;
 	int length;
-	va_list arg;
+	va_list args;
 
+	va_start(args, str);
 	i = 0;
-	length = strlen(args);
-	va_start(arg, args);
-	while (i < length)
+	length = 0;
+	while (str[i] != '\0')
 	{
-		if (args[i] == '%' && (args[i+1] == 'd' || args[i+1] == 'i'))
+		if (str[i] == '%')
 		{
 			i++;
-			ft_putnbr(va_arg(arg, int));
-		}
-		else if (args[i] == '%' && args[i+1] == 's')
-		{
-			i++;
-			ft_putstr(va_arg(arg, char *));
+			if (str[i] == 'c')
+			{
+				ft_putchar(va_arg(args, int));
+				length++;
+			}
+			else if (str[i] == 'd' || str[i] == 'i')
+			{
+				ft_putnbr(va_arg(args, int));
+				length++;
+			}
+			else if (str[i] == 's')
+			{
+				ft_putstr(va_arg(args, char *));
+				length++;
+			}
+			else if (str[i] == '%')
+			{
+				ft_putchar('%');
+				length++;
+			}
 		}
 		else
-			ft_putchar(args[i]);
+		{
+			ft_putchar(str[i]);
+			length++;
+		}
 		i++;
 	}
-	va_end(arg);
-	return (0);
+	va_end(args);
+	return (length);
 }
-
