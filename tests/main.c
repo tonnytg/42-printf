c23e4b10fd12abf8081c60c7687abb3920a2b57c
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-22 01:23:03 -0300

    fix: add new tests

--- tests/main.c
+++ tests/main.c
@@ -2,13 +2,21 @@
 
 int	main(void)
 {
-	ft_printf("Valor: %d\n", 42);
-	ft_printf("Valor2: %s\n", "teste");
 
 
 	int idade = 33;
 	char *nome = "Antonio";
 
-	ft_printf("Me chamo %s e tenho %i anos!\n", nome, idade);
+	ft_printf("Test s e i: Me chamo %s e tenho %i anos!\n", nome, idade);
+	ft_printf("Test s: Valor2: %s\n", "teste");
+
+	ft_printf("Test d e %: Valor com %d%% de desconto\n", 10);
+	ft_printf("Test d: Valor: %d\n", 42);
+
+	ft_printf("Test x: %x\n", 42);
+
+	ft_printf("Test X: %X\n", 42);
+
+	ft_printf("Test p: %p\n", &idade);
 	return (0);
 }
